e46aac1ccb7f8a48b813c1c391fdf25ce0f0ad19
vitor <vitorgabriel181004@gmail.com>
2024-04-11 11:09:07 -0300

    feat: sectionmainhome

--- src/components/Home/SectionMainHome/SectionMainHome.jsx
+++ src/components/Home/SectionMainHome/SectionMainHome.jsx
@@ -6,20 +6,20 @@ import { Link } from 'react-router-dom'
 const SectionMainHome = () => {
   return (
     <div className='SectionMainHome'>
-        <div className='escola-section'><Link to={'/api/escola'}><img src="/escola2.png" alt="escola"/>Escola</Link></div>
+        <Link to={'/api/escola'}><div className='escola-section'><img src="/escola2.png" alt="escola"/>Escola</div></Link>
 
-        <div className='leitura-de-prova-section' ><Link to={'/gabaritos'}><img src="/leitura-de-prova.png" alt="gabaritos"/>Gabaritos</Link></div>
+        <Link to={'/gabaritos'}><div className='leitura-de-prova-section'><img src="/leitura-de-prova.png" alt="gabaritos"/>Gabaritos</div></Link>
         
-        <div className='alunos-section'><Link to={'/api/aluno'}><img src="/aluna.png" alt="aluna"/>Alunos</Link></div>
+        <Link to={'/api/aluno'}><div className='alunos-section'><img src="/aluna.png" alt="aluna"/>Alunos</div></Link>
 
-        <div className='painel'><Link to={'https://app.powerbi.com/view?r=eyJrIjoiOWQ4MDlkZTUtZjI1OS00NzYzLWE4ZDctYzY2NjVlMmY2YWU4IiwidCI6IjVmMTYzNWI1LTk5MmMtNDc2NC05NTViLWQyNTg2ZTQyYjQyNSJ9'} 
-        target="_blank"><img src="/painel.png" alt="dashboard"/>Dashboard</Link></div>
+        <Link to={'https://app.powerbi.com/view?r=eyJrIjoiOWQ4MDlkZTUtZjI1OS00NzYzLWE4ZDctYzY2NjVlMmY2YWU4IiwidCI6IjVmMTYzNWI1LTk5MmMtNDc2NC05NTViLWQyNTg2ZTQyYjQyNSJ9'} 
+        target="_blank"><div className='painel'><img src="/painel.png" alt="dashboard"/>Dashboard</div></Link>
 
         <div className='video-tutorial'><img src="/video-tutorial.png" alt="Tutorial"/>Tutorial</div>
 
         <div className='suporte-online'><img src="/suporte-online.png" alt="suporte"/>Suporte</div>
 
-        <div className='adicionar-usuario-section'><Link to={'/cadastros'}><img src="/adicionar-usuario.png" alt="adicionar novo"/>Adicionar Usuário</Link></div>
+        <Link to={'/cadastros'}><div className='adicionar-usuario-section'><img src="/adicionar-usuario.png" alt="adicionar novo"/>Adicionar Usuário</div></Link>
     </div>
   )
 }
