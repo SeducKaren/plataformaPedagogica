53c6d05e0d6e0bd054f68496299eaf1319e7837e
vitor <vitorgabriel181004@gmail.com>
2024-04-12 13:35:16 -0300

    feat: cadastro-escolasform

--- src/components/Escolas/EscolasForm.jsx
+++ src/components/Escolas/EscolasForm.jsx
@@ -105,7 +105,7 @@ const EscolasForm = () => {
       setCodigoINEP('');
       setEscola('');
       setSigla('');
-      
+      alert('Dados enviados')
       
     } catch (error) {
       console.error('Erro ao enviar os dados:', error);
