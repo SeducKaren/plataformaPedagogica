a87863df63acf0e0afc6bdd035dcb19adc83c74b
vitor <vitorgabriel181004@gmail.com>
2024-04-17 10:37:56 -0300

    feat: teste-cadastro

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -83,7 +83,7 @@ const Login = () => {
     try {
       setLoading(true);
       
-      const response = await api.get(`api/cadastro`);
+      const response = await api.get(`api/cadastro/cpf/${cpf}`);
 
       if (response.data) {
         setUserData(response.data);
