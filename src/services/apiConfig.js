534fca95e18eff5ddb234ecf0aa88351899b3e10
vitor <vitorgabriel181004@gmail.com>
2024-04-19 13:06:15 -0300

    feat: apiconfig

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -4,6 +4,9 @@ const apiUrl = 'https://api-plataforma-pedagogica-zc02.onrender.com';
 
 const api = axios.create({
   baseURL: apiUrl,
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
 
 export default api;
