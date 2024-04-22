7bfae610abddb0fc57ac89f0a4fc2ff90d5cddc1
vitor <vitorgabriel181004@gmail.com>
2024-04-22 11:27:33 -0300

    feat:navbarLogin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Login from './components/Login/Login';
+// import NavbarLogin from './components/Navbar/NavbarLogin/NavbarLogin';
 import Footer from './components/Footer/Footer';
+
 import { Outlet } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      {/* <NavbarLogin /> */}
       <Login />
       <Outlet/>
-      <Footer className="footer" />
+      <Footer />
     </div>
   );
 }
